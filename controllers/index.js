d71fd8ca5a26dfedd09f22111bf76d266fac9021
Yacov Kopel <kokokopel94@gmail.com>
2023-01-20 12:44:37 -0800

    added api route to my store items

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const myStoreItemsRoutes= require('./myStoreItemsRoutes')
 
 router.use('/api', apiRoutes);
+router.use('/', myStoreItemsRoutes);
+
 
 module.exports = router;
--- controllers/myStoreItemsRoutes.js
+++ controllers/myStoreItemsRoutes.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const { Project } = require('../../models');
 
+router.get('/', async (req, res) => {
+  try {
+    res.render('myStoreItems');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const newProject = await Project.create({
