c4eb9b919884ef869d6c4b2a46171a764ef917fa
Yacov Kopel <kokokopel94@gmail.com>
2023-01-21 22:54:45 -0800

    added get route

--- controllers/api/newItemRoutes.js
+++ controllers/api/newItemRoutes.js
@@ -1,13 +1,6 @@
 const router = require('express').Router();
 // // const { User, Project } = require('../models');
 
-// router.get('/', async (req, res) => {
-//   try {
-//     // Get all users, sorted by name
-//     const projectData = await Project.findAll({
-//       include:[{model: User, 
-//       attributes: "name"}]
-//     });
 
 //     // Serialize user data so templates can read it
 //     const projects = projectData.map((project) => project.get({ plain: true }));
