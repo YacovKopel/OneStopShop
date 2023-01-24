de6465729409c8f55fa66aad53902d1865f996f9
luluco92 <117111188+luluco92@users.noreply.github.com>
2023-01-24 16:30:23 -0500

    Update homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
 });
 router.get('/newItem', async (req, res) => {
   try {
-    res.render('newitem');
+    res.render('newitem', { logged_in: req.session.logged_in, user_id: req.session.user_id });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -26,7 +26,7 @@ router.get('/mystore', async (req, res) => {
   console.log(products);
   console.log(newItemData);
         res.render('mystore', {
-          products
+          products, logged_in: req.session.logged_in, user_id: req.session.user_id 
         });
       
     } catch (err) {
@@ -42,7 +42,7 @@ router.get('/marketplace', async (req, res) => {
     product.get({ plain: true })
   );
   console.log(products)
-    res.render('marketplace', {products});
+    res.render('marketplace', {products, logged_in: req.session.logged_in, user_id: req.session.user_id });
   } catch (err) {
     res.status(400).json(err);
   }
