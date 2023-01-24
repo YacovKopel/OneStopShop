2bcc1c2dc0f1980d49a3b70b00d8405c23fcceaf
Yacov Kopel <kokokopel94@gmail.com>
2023-01-24 11:01:42 -0800

    newitem

--- controllers/api/myStoreItemsRoutes.js
+++ controllers/api/myStoreItemsRoutes.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
 const { Product } = require("../../models");
 
-// this post should be linked to the new listing button
-// the market button should only take you to the market web page.
+router.get('/newItem', async (req, res) => {
+  try {
+    const newItemData = await Product.findAll()
+
+    const products = newItemData.map((product) =>
+      product.get({ plain: true })
+    );
+
+    req.session.save(() => {
+      res.render('mystore', {
+        products
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,24 +34,5 @@ router.get('/marketplace', async (req, res) => {
     res.status(400).json(err);
   }
 });
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     const projectData = await Project.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
-
-//     if (!projectData) {
-//       res.status(404).json({ message: 'No project found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(projectData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
 
 module.exports = router;
